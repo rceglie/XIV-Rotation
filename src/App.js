1c1c2a423ff31d0c5b4da5fda2a4441e5890ae4f
rceglie <rceglie@email.unc.edu>
2022-03-01 00:39:16 -0500

    bugs :(

--- src/App.js
+++ src/App.js
@@ -5,13 +5,32 @@ import { Parser } from 'html-to-react'
 import Header from './components/Header';
 import JobSelect from './components/JobSelect';
 
+function getJobs(){
+
+  var ret = []
+
+  const xivapi_request = "https://xivapi.com/ClassJob?columns=ID,Name,Icon,ClassJobCategory.Name,ClassJobCategory.ID,Role,IsLimitedJob,ItemSoulCrystalTargetID,Abbreviation";
+
+  fetch(xivapi_request).then(response => response.json()).then(function (json) {
+    json.Results.forEach((job) => {
+      ret.push(job)
+      console.log(job.Icon)
+    })
+  })
+  
+  return(ret)
+}
+
 function App() {
+
+  const jobs = getJobs()
+
+
   return (
     <html>
       <div className="App">
         <Header />
-        <JobSelect />
-        {Parser().parse(rawHTML)}
+        <JobSelect jobs={jobs}/>
       </div>
     </html>
   );
